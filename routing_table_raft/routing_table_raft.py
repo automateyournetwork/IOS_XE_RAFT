e2547cfd7286d9ff21c5cd792725a900908bae46
automateyournetwork <ptcapo@gmail.com>
2024-04-29 18:19:45 -0400

    another new open source attempt

--- routing_table_raft/routing_table_raft.py
+++ routing_table_raft/routing_table_raft.py
@@ -1,92 +1,55 @@
-import os
-import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
 from datasets import load_dataset
-from peft import LoraConfig, prepare_model_for_kbit_training
-from transformers import (
-        AutoModelForCausalLM,
-        AutoTokenizer,
-        BitsAndBytesConfig,
+from trl import SFTTrainer
+
+MODEL_ID = "microsoft/Phi-3-mini-4k-instruct"
+NEW_MODEL_NAME = "phi3-routing-table"
+DATASET_PATH = "train_dataset.jsonl"
+MAX_SEQ_LENGTH = 2048
+num_train_epochs = 1
+learning_rate = 1.41e-5
+per_device_train_batch_size = 4
+gradient_accumulation_steps = 1
+
+model = AutoModelForCausalLM.from_pretrained(MODEL_ID, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, trust_remote_code=True)
+dataset = load_dataset("json", data_files={"train": DATASET_PATH}, split="train")
+
+EOS_TOKEN=tokenizer.eos_token_id
+
+def process_dataset(mydata):
+    texts = []
+    for entry in mydata["messages"]:
+        role_mapper = {"system": "system\n", "user": "\nuser\n", "assistant": "\nassistant\n"}
+        text = "".join(f"{role_mapper[x['role']]} {x['content']}\n" for x in entry)
+        texts.append(f"{text}{EOS_TOKEN}")
+    return {"text": texts}
+
+dataset = dataset.map(process_dataset, batched=True)
+
+print(dataset['text'][2])
+
+args = TrainingArguments(
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        gradient_checkpointing=True,
+        learning_rate=learning_rate,
+        lr_scheduler_type="cosine",
+        num_train_epochs=num_train_epochs,
+        save_strategy="no",
+        logging_steps=1,
+        output_dir=NEW_MODEL_NAME,
+        optim="paged_adamw_32bit",
+        bf16=True,
     )
-from trl import ORPOConfig, ORPOTrainer, setup_chat_format
 
-# Model
-base_model = "microsoft/Phi-3-mini-4k-instruct"
-new_model = "phi3-routing-table"
-
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_use_double_quant=True,
-)
-
-model = AutoModelForCausalLM.from_pretrained(
-    base_model,
-    quantization_config=bnb_config,
-    device_map="auto",
-    trust_remote_code=True
-)
-
-tokenizer = AutoTokenizer.from_pretrained(base_model)
-
-model, tokenizer = setup_chat_format(model, tokenizer)
-model = prepare_model_for_kbit_training(model)
-
-peft_config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    lora_dropout=0.05,
-    bias="none",
-    task_type="CAUSAL_LM",
-    target_modules=['gate_up_proj', 'down_proj', 'qkv_proj', 'o_proj']
-)
-
-file_path = "train_dataset.jsonl"
-dataset = raw_dataset = load_dataset('json', data_files={'train': file_path}, split='train')
-dataset = dataset.shuffle(seed=42).select(range(253))
-
-def format_for_training(example):
-    # Example transformation to create a 'prompt' field
-    system_statement = example['messages'][0]['content']  # Assuming the first message is always the system role
-    user_question = example['messages'][1]['content']  # Assuming the second message is always the user role
-    example['prompt'] = f"{system_statement} {user_question}"
-    example['labels'] = example['messages'][2]['content']  # Assuming the third message is always the assistant's response
-    return example
-
-# Apply this transformation to each dataset entry
-dataset = dataset.map(format_for_training)
-
-dataset = dataset.train_test_split(test_size=0.01)
-
-print(dataset['train'][0])
-
-orpo_args = ORPOConfig(
-    learning_rate=8e-6,
-    lr_scheduler_type="linear",
-    max_length=512,
-    max_prompt_length=246,
-    beta=0.1,
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
-    gradient_accumulation_steps=4,
-    optim="paged_adamw_8bit",
-    num_train_epochs=1,
-    evaluation_strategy="steps",
-    eval_steps=0.2,
-    logging_steps=1,
-    warmup_steps=10,
-    output_dir=new_model,
-    push_to_hub=True
-)
-
-trainer = ORPOTrainer(
-    model=model,
-    args=orpo_args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["test"],
-    peft_config=peft_config,
-    tokenizer=tokenizer,
-)
+trainer = SFTTrainer(
+        model=model,
+        args=args,
+        train_dataset=dataset,
+        dataset_text_field="text",
+        max_seq_length=MAX_SEQ_LENGTH,
+        formatting_func=process_dataset
+    )
 
 trainer.train()
-trainer.save_model(new_model)
